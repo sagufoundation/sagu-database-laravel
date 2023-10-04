0647ca9f46307f582869bf2905d65a26b7f5ed81
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-10-04 14:29:33 +0900

    ADDED: category description

--- database/seeders/Library/CateegoriesSeeder.php
+++ database/seeders/Library/CateegoriesSeeder.php
@@ -16,11 +16,11 @@ class CateegoriesSeeder extends Seeder
     public function run()
     {
         Catagories::create([ 'name' => 'Fashion','slug' => 'fashion','description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]);
-        
-        Catagories::create([ 'name' => 'Health','slug' => 'health','description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]);
 
-        Catagories::create([ 'name' => 'Natural','slug' => 'natural','description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]);
+        Catagories::create([ 'name' => 'Health','slug' => 'health','description' => "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]);
 
-        Catagories::create([ 'name' => 'Education','slug' => 'education','description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]);
+        Catagories::create([ 'name' => 'Natural','slug' => 'natural','description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"]);
+
+        Catagories::create([ 'name' => 'Education','slug' => 'education','description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry."]);
     }
 }
