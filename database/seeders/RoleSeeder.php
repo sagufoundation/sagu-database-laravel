13185bcf218994ed432cdde1a4ff5a0c9c3a24d8
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-10-05 12:49:57 +0900

    UPDATED

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -36,31 +36,35 @@ class RoleSeeder extends Seeder
             'name' => 'administrator',
             'guard_name' => 'web',
             'display_name' => 'Administrator',
-            'description' => 'Administrator Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+            'description' => 'Account with this role can access all the features. Able to view, create, edit, delete all the datas.',
         ]);
+
         Role::create([
             'name' => 'users',
             'guard_name' => 'web',
             'display_name' => 'Users',
-            'description' => 'Users Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+            'description' => 'Able to view some features : View and update profile, view books, manage loan logs.',
         ]);
-        Role::create([
-            'name' => 'guest',
-            'guard_name' => 'web',
-            'display_name' => 'Guest',
-            'description' => 'Guest Lorem Ipsum is simply dummy text of the printing and typesetting industry',
-        ]);
+
         Role::create([
             'name' => 'student',
             'guard_name' => 'web',
             'display_name' => 'Student',
-            'description' => 'Student Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+            'description' => 'Able to view some features : View and update profile, view books, manage loan logs.',
         ]);
+
+        Role::create([
+            'name' => 'guest',
+            'guard_name' => 'web',
+            'display_name' => 'Guest',
+            'description' => 'Able to view some features and do not able to manage or update any information.',
+        ]);
+
         Role::create([
             'name' => 'librarian',
             'guard_name' => 'web',
             'display_name' => 'Librarian',
-            'description' => 'Librarian Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+            'description' => 'Able to view and manage some features: View, create, edit, delete books and also manage the loan logs.',
         ]);
     }
 }
