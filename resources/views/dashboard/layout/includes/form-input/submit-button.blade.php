cd6d1d75d5c35f46c8a0fc36c8e4b6957e7b4219
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-10-11 13:36:28 +0900

    update button submit

--- resources/views/dashboard/layout/includes/form-input/submit-button.blade.php
+++ resources/views/dashboard/layout/includes/form-input/submit-button.blade.php
@@ -2,21 +2,15 @@
     <div class="col-12">
         <div class="card">
             <div class="card-body">
-                <div class="col-3">
-                    <div class="button-list pe-xl-4 d-grid">
-                        
-                        <button  type="submit" class="btn btn-lg btn-primary waves-effect waves-light">
-                            <i class="fa-solid fa-save mr-1"></i> Save
-                        </button>
-                        <!-- end item -->
-            
-                        <a href="{{ url(Request::segment(1).'/'.Request::segment(2)) }}" class="btn btn-lg btn-outline-primary">
-                            <i class="fa-solid fa-reply"></i> Back to {{ ucfirst(Request::segment(2)) }} 
-                        </a>
-                        <!-- end item -->
-
-                    </div>
-                </div> <!-- end col -->
+                <button  type="submit" class="btn btn-lg btn-primary waves-effect waves-light">
+                    <i class="fa-solid fa-save mr-1"></i> Save
+                </button>
+                <!-- end item -->
+    
+                <a href="{{ url(Request::segment(1).'/'.Request::segment(2)) }}" class="btn btn-lg btn-outline-primary">
+                    <i class="fa-solid fa-reply"></i> Back to {{ ucfirst(Request::segment(2)) }} 
+                </a>
+                <!-- end item -->
             </div>
         </div>
     </div>
