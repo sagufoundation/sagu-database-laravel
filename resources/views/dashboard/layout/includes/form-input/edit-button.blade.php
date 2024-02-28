2822c214d1049bbc94f9dc78a19de554944a6ee4
Samuel Bosawer <zam.bosawer@gmail.com>
2024-02-28 14:44:39 +0900

    Update button edit

--- resources/views/dashboard/layout/includes/form-input/edit-button.blade.php
+++ resources/views/dashboard/layout/includes/form-input/edit-button.blade.php
@@ -5,13 +5,13 @@
                 <div class="col">
                     <div class="">
 
-                        <a href="{{ url(Request::segment(1).'/'.Request::segment(2). '/edit',  $data->id) }}" class="btn btn-lg btn-primary">
+                        <a href="{{ url(Request::segment(1).'/'.Request::segment(2). '/edit',Request::segment(4)) }}" class="btn btn-lg btn-primary">
                             <i class="fa-solid fa-pen-to-square"></i> Edit
                         </a>
                         <!-- end item -->
-            
+
                         <a href="{{ url(Request::segment(1).'/'.Request::segment(2)) }}" class="btn btn-lg btn-outline-primary">
-                            <i class="fa-solid fa-reply"></i> Back to {{ ucfirst(Request::segment(2)) }} 
+                            <i class="fa-solid fa-reply"></i> Back to {{ ucfirst(Request::segment(2)) }}
                         </a>
                         <!-- end item -->
                     </div>
