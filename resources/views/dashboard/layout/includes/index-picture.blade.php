5ef2bf3b8d32aab1cc0e684f31ace67469fed4ad
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-02-29 15:35:02 +0900

    update picture url

--- resources/views/dashboard/layout/includes/index-picture.blade.php
+++ resources/views/dashboard/layout/includes/index-picture.blade.php
@@ -1,10 +1,24 @@
+@if (ENV('APP_ENV') == 'local')
+
+<td class="p-0" width="100px">
+    @if (!empty($data->picture))
+        <img src="https://dbsf.sagufoundation.org/{{ $data->picture }}" alt="Profile picture" class="img img-fluid w-100">
+    @elseif(!empty($student->users->picture))
+        <img src="{{ asset($student->users->picture) }}" alt="Profile picture" class="img img-fluid w-100">
+        <img src="https://dbsf.sagufoundation.org/{{ $data->users->picture }}" alt="Profile picture" class="img img-fluid w-100">
+    @else
+        <img src="{{ asset('images/' . Request::segment(2) . '/00.jpg') }}" alt="Profile picture not found" class="img w-100">
+    @endif
+</td>
+
+@else
 <td class="p-0" width="100px">
     @if (!empty($data->picture))
         <img src="{{ asset($data->picture) }}" alt="Profile picture" class="img img-fluid w-100">
     @elseif(!empty($student->users->picture))
         <img src="{{ asset($student->users->picture) }}" alt="Profile picture" class="img img-fluid w-100">
     @else
-        <img src="{{ asset('images/' . Request::segment(2) . '/00.jpg') }}" alt="Profile picture not found"
-            class="img w-100">
+        <img src="{{ asset('images/' . Request::segment(2) . '/00.jpg') }}" alt="Profile picture not found" class="img w-100">
     @endif
 </td>
+@endif
